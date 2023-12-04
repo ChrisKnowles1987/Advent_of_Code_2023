e9d696f042630e338560713014df7250dfdcbb9d
ChrisKMBP <chrisknowles1987@me.com>
2023-12-04 14:43:43 +0000

    Day one destroyed

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,4 +1,5 @@
 numbers = '0123456789'
+letters = 'otfsen' #first letter of each possible wrd number
 words_dict = {
     "one": "1",
     "two": "2",
@@ -11,26 +12,39 @@ words_dict = {
     "nine": "9"
     }   
 array =[]
-    
+words = words_dict.keys()
 def main():
-    
+    selected_chars = ''
     with open('calibration.txt', 'r') as file:
         lines = [line.strip() for line in file]
         for line in lines:
-            for word in words_dict.keys():
-                value = words_dict[word]
-                if word in line:
-                    line = line.replace(word,value)
-                    print(line)
-            
-            selected_chars = ''.join([c for c in line if c in numbers])
+            selected_chars = []
+            for index, c in enumerate(line):
+                
+                if c in numbers:
+                    selected_chars.append(int(c))
+                    
+                if c in letters:
+                    
+                    for word in words_dict.keys():
+                        x = word[0]
+                        start_index = line[index:].find(word) + index
+                        value = words_dict[word]
+                        
+                   
+                        if  (x == c) and (start_index == index): #and (line[index+1] == word[1]):
+                         selected_chars.append(int(value))
+                         
+            # print(selected_chars)
             first_char = selected_chars[0]
             last_char = selected_chars[-1]
-            number = first_char + last_char
+            number = str(first_char) + str(last_char)
             array.append(int(number))
+            print(f'{line} {number}')
+    
             
-        print(sum(array))
-    return (sum(array))
+    print(sum(array))
+    # return (sum(array))
 
 if __name__ == '__main__':
     main()
