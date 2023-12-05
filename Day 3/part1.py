a496a95f115ae3f17090485038c2fa47879dcc62
ChrisKMBP <chrisknowles1987@me.com>
2023-12-05 07:56:57 +0000

    resolve conflicts after file name change

---
+++ Day 3/part1.py
@@ -0,0 +1,10 @@
+'''
+1 convert input to array of arrays
+2 find pos of charcacters
+iterate over character pos and check neighbouirs
+try new array.append(int(neigbour)) else return
+profit?
+'''
+
+with open('input.txt') as file:
+    
