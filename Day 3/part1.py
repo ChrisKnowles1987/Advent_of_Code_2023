20ab928a15020ecd1dfc538ad50052b07899b8a2
ChrisKMBP <chrisknowles1987@me.com>
2023-12-05 09:15:49 +0000

    added structure for solution in comments

--- Day 3/part1.py
+++ Day 3/part1.py
@@ -6,5 +6,36 @@ try new array.append(int(neigbour)) else return
 profit?
 '''
 
-with open('input.txt') as file:
+with open('input.txt') as input:
+    file = input.read()
+    engine_schematic = [list(line) for line in file.split('\n')]
+
+def get_char_coords(array):
+    #needs to get coords of only symbols
+    coordinates = []
+    for x_index, x in enumerate(array):
+        for y_index, y in enumerate(x):
+            if y != '.':
+                
+                coordinates.append(tuple([x_index, y_index]))
+    print(coordinates)
     
+def neighbours():
+    
+    directions = [
+    (0 , +1),   # right
+    (0 , -1),   # left
+    (+1 , 0),   # up
+    (-1 , 0),   # down
+    (+1 , +1),  # up right
+    (-1 , -1),  # down left
+    (+1 , -1),  # up left
+    (-1. , +1)] # down right
+        
+    for direction in directions:
+        
+    #this should find the coordinates of neighbours that contain a number, 
+    # then move to the start of the number and return a concat string of all the numbers in the sequence
+   
+# get_char_coords(engine_schematic) 
+       
