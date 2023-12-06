8821925789d2578e58e68e5286c4bc21b6e0f02a
ChrisKMBP <chrisknowles1987@me.com>
2023-12-06 21:16:48 +0000

    Day 3 part 2 in 25 min first try like a boss

--- Day 3/part2.py
+++ Day 3/part2.py
@@ -1,33 +1,33 @@
+'''A gear is any * symbol that is adjacent to exactly two part numbers. 
+Its gear ratio is the result of multiplying those two numbers together.
+
+find just *
+stop appending found numbers if more than 2
+multiply found numbers[0],fund numbers[1] and append to new array
+sum new array
 '''
-convert input to array of arrays
-find all chars in array
-find pos of chars
-iterate over char pos and check neighbouirs for numbers
-if number get start and return number to array, remove number friom array to avoid duplicates
-sum numbers
-'''
-not_chars = '0123456789.'
+not_gears = '0123456789.'
 with open('input.txt') as input:
     file = input.read()
     engine_schematic = [list(line) for line in file.split('\n')]
 
-def get_chars(array):
-    chars = []
-    for x in array:
-        for y in x:
-            if y not in ([item for item in list(not_chars)]) and y not in (chars) :
-                chars.append(y)
+# def get_gears(array):
+#     gears = []
+#     for x in array:
+#         for y in x:
+#             if y not in ([item for item in list(not_gears)]) and y not in gears :
+#                 gears.append(y)
                 
-    return chars
+#     return gears
 
 def get_char_coords(array):
-    chars = get_chars(array)
+    gears = '*'
     coordinates = []
     for x_index, x in enumerate(array):
         for y_index, y in enumerate(x):
-            if y in [item for item in list(chars)]:
+            if y == gears:
                 coordinates.append(tuple([x_index, y_index]))
-                
+    print(coordinates)           
     return coordinates
     
 def neighbours(schematic, coords):
@@ -43,8 +43,10 @@ def neighbours(schematic, coords):
     (-1 , +1)] # down right
         
     array = schematic
-    found_numbers = []
+    
+    gear_ratios = []
     for x, y in coords:
+        found_numbers = []
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
             if 0 <= nx < len(array) and 0 <= ny < len(array[0]) and array[nx][ny].isdigit():
@@ -58,7 +60,10 @@ def neighbours(schematic, coords):
                     array[nx][ny]=array[nx][ny].replace((array[nx][ny]), '.')
                     ny += 1                    
                 found_numbers.append(int(found_number))
-    print(sum(found_numbers))
+                if len(found_numbers) >= 2:
+                    gear_ratios.append((found_numbers[0] * found_numbers[1]))
+                    
+    print(f'gear_ratios: {sum(gear_ratios)}')
     return found_numbers
 
 coords = get_char_coords(engine_schematic)
