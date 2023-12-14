a84460d1f0753d2ac570e7855a2f5e73df13e74b
ChrisKMBP <chrisknowles1987@me.com>
2023-12-14 17:23:08 +0000

    encounter  some incredible off by one bugs

--- Day 5/part1.py
+++ Day 5/part1.py
@@ -50,7 +50,7 @@ def mapIO(seed, map):
        rng = entry[2]
        max_source = source + rng
        
-       if source <= seed <= max_source:
+       if source <= seed < max_source:
            #diff source and seed and add to 
            seed = (seed-source + (destination))
            return seed
@@ -58,11 +58,22 @@ def mapIO(seed, map):
            seed = seed
     return seed
 
-
-
-
-
-with open('input.txt') as file:
+def mapIO_2(location, map):
+    for entry in map:
+       destination = entry[0]
+       source =  entry[1]
+       rng = entry[2]
+       max_destination = destination + rng
+       
+       if destination <= location <= max_destination:
+           location = (location-destination + (source))
+           return location
+       else:
+           location = location
+    return location
+    
+            
+with open('test.txt') as file:
     file = file.read()
     lines = file.split('\n')
     
@@ -86,16 +97,54 @@ temperature_to_humidity_map,
 humidity_to_location_map,
 ]
 
-# print(f'test: {mapIO(81, water_to_light_map)}')
 
-map_steps = []
-for seed in seeds:
+def main(seeds):
+        
+    map_steps = []
+    for seed in seeds:
+        
+        counter = 0
+        old_seed = seed
+        while counter < len(map_list):
+            new_seed = mapIO(old_seed,map_list[counter])
+            old_seed = new_seed
+            counter += 1
+        map_steps.append(new_seed)
     
-    counter = 0
-    old_seed = seed
-    while counter < len(map_list):
-        new_seed = mapIO(old_seed,map_list[counter])
-        old_seed = new_seed
-        counter += 1
-    map_steps.append(new_seed)
-print(min(map_steps))
+    return map_steps
+
+def main_2():
+    # print('Running part 2')
+    seed_start = seeds[0::2]
+    seed_range = seeds[1::2] 
+    seed_found = False
+    location = 0
+    old_location = 0
+    while  seed_found == False:
+        counter = len(map_list) -1
+        location += 1
+        old_location = location
+        # print(location)
+        while counter >= 0:
+            new_location = mapIO_2(old_location, map_list[counter])
+            # print(f'map{counter} value:{new_location}')
+            old_location = new_location
+            counter -= 1
+        # print(location)
+        for x in range(len(seed_start)): 
+            seed_min = seed_start[x] 
+            seed_max = seed_start[x] + seed_range[x]
+            # print(f'{range(seed_min,seed_max)}')  
+            if seed_min <= new_location  < seed_max:
+                seed_found = True
+                return location
+   
+
+# print(main_2())
+# part1 = main(seeds)
+# print(f'part1: {min(part1)}')
+# print(f'Part2: {main_2()}')
+
+
+
+
