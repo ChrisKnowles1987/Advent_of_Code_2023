a34fefcd4db1afbd06309d1045b0b97ef3aec529
ChrisKMBP <chrisknowles1987@me.com>
2023-12-16 16:18:04 +0000

    modifiy zsh script

---
+++ Day7/Day7.py
@@ -0,0 +1,13 @@
+with open('input.txt', 'r') as file:
+    lines = file.readlines
+
+def part1():
+    
+    return
+
+def part2():
+    
+    return
+
+print(f'part1: {part1()}')
+print(f'part2: {part2()}')
