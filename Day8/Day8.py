3fe94e4f22a69e321c6a179be25e19dfad54afd9
ChrisKMBP <chrisknowles1987@me.com>
2023-12-22 16:14:23 +0000

    day 8 part 2

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,5 +1,4 @@
-#look for py test
-
+import math
 
 with open('input.txt', 'r') as file:
     file = file.read().split('\n')
@@ -13,8 +12,7 @@ def get_directions():
             directions.append(1)
     return directions
 
-def part1():
-    def get_maps(): #call maps with maps['key'][0/1]
+def get_maps(): #call maps with maps['key'][0/1]
         maps ={}
         for line in file[2::]:
             key, value = line.split('=')
@@ -22,8 +20,9 @@ def part1():
             value = value.strip("( )")
             value = tuple(value.split(', '))
             maps[key] = value
-        return maps 
-   
+        return maps
+
+def part1():
     def run_map(directions, maps):
         counter = 0
         score = 1
@@ -42,8 +41,44 @@ def part1():
     return answer
 
 def part2():
-    # Your code for part 2
-    return
+    directions = get_directions()
+    maps = get_maps()
+    
+    def get_start_nodes(maps):
+        start_nodes = [key for key in maps if key.endswith('A')]
+        return start_nodes
+    
+    def get_cycles(directions, maps): #gets number of steps each start takes to reach the end
+        counter = 0
+        cycles = []
+        nodes = get_start_nodes(maps)
+        for node in nodes:
+            cycle = 1
+            while not node.endswith('Z'):
+                node = maps[node][directions[counter]]
+                counter +=1
+                cycle += 1
+                if counter == len(directions):
+                    counter = 0
+            cycles.append((cycle-1))
+        return cycles
+    
+    def run_cycles(): #lowest common multipler for the first pair, then factor in each cycle int he list
+        #lcm is the product of 2 numbers divided by their lowest common denominator
+        def get_lcm(a,b):
+            lcm = abs(a * b) // math.gcd(a,b)
+            return lcm
+            
+        cycles = get_cycles(directions,maps)
+        cycles.sort()
+        
+        #start at cycles[0]
+        lcm = cycles[0]
+        for cycle in cycles[1:]:
+            lcm = get_lcm(lcm, cycle) #adds each cycle int he list and calculates the new lcm  
+        return lcm              
+    answer = run_cycles()       
+    return answer
 
 print(f'part1: {part1()}')
 
